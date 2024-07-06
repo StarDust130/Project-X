cd0c4654bc95f8a922ba8f8f7cd4ad863621c54b
ChandraShekhar <csyadav0513@gmail.com>
2024-07-06 12:07:47 +0530

    Add Assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import { ModeToggle } from "@/components/ModeToggle";
-
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Hello
-      <ModeToggle />
+    <main className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[469px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+        </div>
+      </section>
     </main>
   );
 }
