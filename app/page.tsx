7161eabda2dd131ab36a1a7e0fa60ae1839e34bd
ChandraShekhar <csyadav0513@gmail.com>
2024-07-06 12:25:16 +0530

    Home Page Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const year = new Date().getFullYear();
   return (
     <main className="flex h-screen max-h-screen">
+      {/* //TODO: ADD OTP verification */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[469px]">
           <Image
@@ -11,11 +14,38 @@ export default function Home() {
             alt="patient"
             width={1000}
             height={1000}
-            className="mb-12 h-10 w-fit"
+            className="mb-12 h-10 w-fit pointer-events-none"
           />
           <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between ">
+            <p>
+              © {year}{" "}
+              <Link
+                href={"https://github.com/StarDust130"}
+                target="_blank"
+                className="justify-items-end text-yellow-300 hover:text-yellow-500 xl:text-left"
+              >
+                StarDust🌟
+              </Link>{" "}
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%] pointer-events-none"
+      />
     </main>
   );
+}
+
+{
 }
